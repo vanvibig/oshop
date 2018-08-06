55d8e5016c5aab3771fa5c7305859040c62b355f
vi <dev.facereal@gmail.com>
2018-08-06 13:10:57 +0700

    292 Protecting the Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import {AdminProductsComponent} from './admin/admin-products/admin-products.comp
 import {AdminOrdersComponent} from './admin/admin-orders/admin-orders.component';
 import {RouterModule} from '@angular/router';
 import {LoginComponent} from './login/login.component';
+import {AuthGuard} from './auth-guard.service';
 
 @NgModule({
     declarations: [
@@ -43,12 +44,14 @@ import {LoginComponent} from './login/login.component';
             {path: '', component: HomeComponent},
             {path: 'products', component: ProductsComponent},
             {path: 'shopping-cart', component: ShoppingCartComponent},
-            {path: 'check-out', component: CheckOutComponent},
-            {path: 'order-success', component: OrderSuccessComponent},
-            {path: 'my/orders', component: MyOrdersComponent},
             {path: 'login', component: LoginComponent},
-            {path: 'admin/products', component: AdminProductsComponent},
-            {path: 'admin/orders', component: AdminOrdersComponent},
+
+            {path: 'check-out', component: CheckOutComponent, canActivate: [AuthGuard]},
+            {path: 'order-success', component: OrderSuccessComponent, canActivate: [AuthGuard]},
+            {path: 'my/orders', component: MyOrdersComponent, canActivate: [AuthGuard]},
+
+            {path: 'admin/products', component: AdminProductsComponent, canActivate: [AuthGuard]},
+            {path: 'admin/orders', component: AdminOrdersComponent, canActivate: [AuthGuard]},
         ])
 
     ],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyCpfBmetZa6nyt4RbUSK-gGTdb0gPjWV6E",
-    authDomain: "kvoshop.firebaseapp.com",
-    databaseURL: "https://kvoshop.firebaseio.com",
-    projectId: "kvoshop",
-    storageBucket: "kvoshop.appspot.com",
-    messagingSenderId: "657690934840"
+    apiKey: 'AIzaSyCpfBmetZa6nyt4RbUSK-gGTdb0gPjWV6E',
+    authDomain: 'kvoshop.firebaseapp.com',
+    databaseURL: 'https://kvoshop.firebaseio.com',
+    projectId: 'kvoshop',
+    storageBucket: 'kvoshop.appspot.com',
+    messagingSenderId: '657690934840'
   }
 };
 
