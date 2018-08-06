ba07dc8e90cc6dab2ec8c0d28cfe50d2ea7ceb3b
vi <dev.facereal@gmail.com>
2018-08-06 10:52:44 +0700

    291 Extracting a Service

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {AuthService} from '../auth.service';
 
 @Component({
-  selector: 'bs-navbar',
-  templateUrl: './bs-navbar.component.html',
-  styleUrls: ['./bs-navbar.component.css']
+    selector: 'bs-navbar',
+    templateUrl: './bs-navbar.component.html',
+    styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent implements OnInit {
+export class BsNavbarComponent {
 
-  constructor() { }
+    constructor(private authService: AuthService) {
+    }
 
-  ngOnInit() {
-  }
+    logout() {
+        this.authService.logout();
+    }
 
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AngularFireAuth} from 'angularfire2/auth';
+import * as firebase from 'firebase';
+import {AuthService} from '../auth.service';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+    constructor(
+        private authService: AuthService
+    ) {
+    }
 
-  ngOnInit() {
-  }
-
+    login() {
+        this.authService.login();
+    }
 }
