740020bcc76613d174d8f8a378cbdadb33404797
vi <dev.facereal@gmail.com>
2018-08-11 19:24:11 +0700

    fix auth guard service

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,6 +1,8 @@
 import {Injectable} from '@angular/core';
 import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
 import {AuthService} from './auth.service';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
@@ -10,17 +12,19 @@ export class AuthGuard implements CanActivate {
     constructor(private authService: AuthService, private router: Router) {
     }
 
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        let ret = false;
-        this.authService.user$.subscribe(
-            user => {
-                if (user) {
-                   ret = true;
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+        // let ret = false;
+        return this.authService.user$.pipe(
+            map(
+                user => {
+                    if (user) {
+                        return true;
+                    }
+                    this.router.navigate(['/login'], {queryParams: {returnUrl: state.url}});
+                    return false;
                 }
-                this.router.navigate(['/login'], {queryParams: {returnUrl: state.url}});
-                ret = false;
-            }
+            )
         );
-        return ret;
+        // return ret;
     }
 }
