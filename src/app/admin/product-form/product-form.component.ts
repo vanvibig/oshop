86139240c85d52d70225eadf2c9bb67c47bfc0bb
vi <dev.facereal@gmail.com>
2018-09-06 00:00:11 +0700

    301 Populating the Categories Drop-down List | 7m40s | not finish

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Observable } from "rxjs";
+import { CategoryService } from "./../../category.service";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-product-form',
-  templateUrl: './product-form.component.html',
-  styleUrls: ['./product-form.component.css']
+    selector: "app-product-form",
+    templateUrl: "./product-form.component.html",
+    styleUrls: ["./product-form.component.css"]
 })
 export class ProductFormComponent implements OnInit {
+    categories$: Observable<any>;
+    keys = [];
 
-  constructor() { }
+    constructor(categoryService: CategoryService) {
+        this.categories$ = categoryService.getCategories();
+        categoryService.getCategoriesKey().subscribe(res => {
+            this.keys = res;
+        });
+    }
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {}
 }
---
+++ src/app/category.service.ts
@@ -0,0 +1,17 @@
+import { AngularFireDatabase } from "angularfire2/database";
+import { Injectable } from "@angular/core";
+
+@Injectable({
+    providedIn: "root"
+})
+export class CategoryService {
+    constructor(private db: AngularFireDatabase) {}
+
+    getCategories() {
+      return this.db.list('/categories').valueChanges();
+    }
+
+    getCategoriesKey() {
+      return this.db.list('/categories').snapshotChanges();
+    }
+}
