ed1be27e2381de0d45dd424a301964c921b1db7b
vi <dev.facereal@gmail.com>
2018-10-05 11:52:22 +0700

    329 Creating a Shopping Cart

--- src/app/product-cart/product-cart.component.ts
+++ src/app/product-cart/product-cart.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Product} from '../models/product';
+import {ShoppingCartService} from '../shopping-cart.service';
 
 @Component({
     selector: 'app-product-cart',
@@ -11,10 +12,27 @@ export class ProductCartComponent implements OnInit {
     @Input('product') product: Product;
     @Input('show-action') showAction = true;
 
-    constructor() {
+    constructor(
+        private cartService: ShoppingCartService
+    ) {
     }
 
     ngOnInit() {
+    }
+
+    addToCart(product: Product) {
+        const cartId = localStorage.getItem('cartId');
+        if (!cartId) {
+            this.cartService.create().then(
+                (res) => {
+                    localStorage.setItem('cartId', res.key);
+
+                    // add product to cart
+                }
+            );
+        } else {
+            // add product to cart
+        }
     }
 
 }
