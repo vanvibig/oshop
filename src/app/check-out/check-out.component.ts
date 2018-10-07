4706fa27ff524d9094347451ca87525421e3218c
vi <dev.facereal@gmail.com>
2018-10-07 22:00:22 +0700

    355 Refactoring Extract an Order Model

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -4,6 +4,7 @@ import {ShoppingCart} from '../models/shopping-cart';
 import {Subscription} from 'rxjs';
 import {OrderService} from '../order.service';
 import {AuthService} from '../auth.service';
+import {Order} from '../models/order';
 
 @Component({
     selector: 'app-check-out',
@@ -39,24 +40,7 @@ export class CheckOutComponent implements OnInit, OnDestroy {
     }
 
     placeOrder() {
-        let order = {
-            userId: this.userId,
-            datePlaced: new Date().getTime(),
-            shipping: this.shipping,
-            items: this.cart.items.map(
-                item => {
-                    return {
-                        product: {
-                            title: item.title,
-                            imageUrl: item.imageUrl,
-                            price: item.price
-                        },
-                        quantity: item.quantity,
-                        totalPrice: item.totalPrice
-                    };
-                }
-            )
-        };
+        let order = new Order(this.userId, this.shipping, this.cart);
         this.orderService.storeOrder(order);
     }
 
