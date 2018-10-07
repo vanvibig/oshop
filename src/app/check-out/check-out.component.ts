040b5a985b7f3dcb82feb39a811620021b06968c
vi <dev.facereal@gmail.com>
2018-10-07 21:41:28 +0700

    356 Redirecting the User to the Order Success Page

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ShoppingCartService} from '../shopping-cart.service';
+import {ShoppingCart} from '../models/shopping-cart';
+import {Subscription} from 'rxjs';
+import {OrderService} from '../order.service';
 
 @Component({
-  selector: 'app-check-out',
-  templateUrl: './check-out.component.html',
-  styleUrls: ['./check-out.component.css']
+    selector: 'app-check-out',
+    templateUrl: './check-out.component.html',
+    styleUrls: ['./check-out.component.css']
 })
-export class CheckOutComponent implements OnInit {
+export class CheckOutComponent implements OnInit, OnDestroy {
+    shipping = {};
+    private cart: ShoppingCart;
+    subscription: Subscription;
 
-  constructor() { }
+    constructor(
+        private cartService: ShoppingCartService,
+        private orderService: OrderService
+    ) {
+    }
 
-  ngOnInit() {
-  }
+    async ngOnInit() {
+        let cart$ = await this.cartService.getCart();
+        this.subscription = cart$.subscribe(cart => this.cart = cart);
+    }
+
+    ngOnDestroy(): void {
+        this.subscription.unsubscribe();
+    }
+
+    placeOrder() {
+        let order = {
+            datePlaced: new Date().getTime(),
+            shipping: this.shipping,
+            items: this.cart.items.map(
+                item => {
+                    return {
+                        product: {
+                            title: item.title,
+                            imageUrl: item.imageUrl,
+                            price: item.price
+                        },
+                        quantity: item.quantity,
+                        totalPrice: item.totalPrice
+                    }
+                }
+            )
+        };
+        this.orderService.storeOrder(order);
+    }
 
 }
